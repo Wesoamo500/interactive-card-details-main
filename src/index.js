f73ce9e3b03ae9aefa598467500e272dd8190d1a
manan-eth <abdul.suleman@amalitech.org>
2023-04-23 17:00:06 +0000

    transpiled script

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ var cardNameInput = document.querySelector('#name');
 var cardName = document.querySelector('.name');
 var cardNumberInput = document.querySelector('#card-number');
 var cardNumber = document.querySelector('.card-number');
+var monthInput = document.querySelector('#month');
+var yearInput = document.querySelector('#year');
+var cvcInput = document.querySelector('#cvc');
+var month = document.querySelector('.mon');
+var year = document.querySelector('.yr');
+var cvc = document.querySelector('#output');
 cardNameInput.addEventListener('input', function (e) {
     var target = e.target;
     cardName.innerText = target.value;
@@ -9,4 +15,16 @@ cardNameInput.addEventListener('input', function (e) {
 cardNumberInput.addEventListener('input', function (e) {
     var target = e.target;
     cardNumber.innerText = target.value;
+});
+monthInput.addEventListener('input', function (e) {
+    var target = e.target;
+    month.innerText = target.value;
+});
+yearInput.addEventListener('input', function (e) {
+    var target = e.target;
+    year.innerText = target.value;
+});
+cvcInput.addEventListener('input', function (e) {
+    var target = e.target;
+    cvc.innerText = target.value;
 });
