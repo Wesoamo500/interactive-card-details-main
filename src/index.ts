8b8cd822e96fd0ab90ac68db089fd75919487a63
manan-eth <abdul.suleman@amalitech.org>
2023-04-23 16:59:49 +0000

    complete the live update of cc card as input

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ const cardNameInput = document.querySelector('#name') as HTMLInputElement
 const cardName = document.querySelector('.name') as HTMLHeadingElement
 const cardNumberInput = document.querySelector('#card-number') as HTMLInputElement
 const cardNumber = document.querySelector('.card-number') as HTMLHeadingElement
+const monthInput = document.querySelector('#month') as HTMLInputElement
+const yearInput = document.querySelector('#year') as HTMLInputElement
+const cvcInput = document.querySelector('#cvc') as HTMLInputElement
+const month = document.querySelector('.mon') as HTMLHeadingElement
+const year = document.querySelector('.yr') as HTMLHeadingElement
+const cvc = document.querySelector('#output') as HTMLOutputElement
+
 
 
 
@@ -13,4 +20,16 @@ cardNameInput.addEventListener('input',(e:Event)=>{
 cardNumberInput.addEventListener('input',(e:Event)=>{
     const target = e.target as HTMLInputElement;
     cardNumber.innerText = target.value;
+})
+monthInput.addEventListener('input',(e:Event)=>{
+    const target = e.target as HTMLInputElement;
+    month.innerText = target.value;
+})
+yearInput.addEventListener('input',(e:Event)=>{
+    const target = e.target as HTMLInputElement;
+    year.innerText = target.value;
+})
+cvcInput.addEventListener('input',(e:Event)=>{
+    const target = e.target as HTMLInputElement;
+    cvc.innerText = target.value;
 })
